1f4b029ab7a1266ee4551fd88fd6f9b70c54f488
Alejo Rondon <blackphotografy@gmail.com>
2020-07-16 10:32:59 -0500

    [LOAD] Dinosaur exposition test server of files(Like CDN)

--- js/exposition-dinosaurs-main.js
+++ js/exposition-dinosaurs-main.js
@@ -1,3 +1,4 @@
+//-> https://stackoverflow.com/questions/1248081/how-to-get-the-browser-viewport-dimensions
 const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
 console.log(`vw`, vw)
 const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
